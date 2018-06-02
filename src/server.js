a37c78aa1beb12fffe316945009bdd43e24ac803
pdesupinski <paul.b.desupinski.19@dartmouth.edu>
2018-06-01 22:36:55 -0400

    fixing heroku

--- src/server.js
+++ src/server.js
@@ -11,6 +11,39 @@ import getCode from './translate';
 
 dotenv.config({ silent: true });
 
+// initialize
+const app = express();
+
+// enable/disable cross origin resource sharing if necessary
+app.use(cors());
+
+// enable/disable http request logging
+app.use(morgan('dev'));
+
+// enable only if you want templating
+app.set('view engine', 'ejs');
+
+// enable only if you want static assets from folder static
+app.use(express.static('static'));
+
+// this just allows us to render ejs from the ../app/views directory
+app.set('views', path.join(__dirname, '../src/views'));
+
+// enable json message body for posting data to API
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// default index route
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+// START THE SERVER
+// =============================================================================
+const port = process.env.PORT || 9090;
+app.listen(port);
+
 let language = 'en'; // English
 
 // botkit controller
@@ -70,38 +103,5 @@ controller.hears(['paulbot', 'paulbot,', 'Paulbot,', 'Paulbot,', 'PaulBot', 'Pau
 controller.on('outgoing_webhook', (bot, message) => {
   bot.replyPublic(message, 'im awake');
 });
-
-// initialize
-const app = express();
-
-// enable/disable cross origin resource sharing if necessary
-app.use(cors());
-
-// enable/disable http request logging
-app.use(morgan('dev'));
-
-// enable only if you want templating
-app.set('view engine', 'ejs');
-
-// enable only if you want static assets from folder static
-app.use(express.static('static'));
-
-// this just allows us to render ejs from the ../app/views directory
-app.set('views', path.join(__dirname, '../src/views'));
-
-// enable json message body for posting data to API
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-// default index route
-app.get('/', (req, res) => {
-  res.send('hi');
-});
-
-// START THE SERVER
-// =============================================================================
-const port = process.env.PORT || 9090;
-app.listen(port);
 
 console.log(`listening on: ${port}`);
